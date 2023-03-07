d241a81c27176712e40e1163b4484a428cb175cc
Jinseok Seo (Jason Jin) <jinseok9338@gmail.com>
2023-03-08 08:08:31 +0900

    delete some files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,26 +3,11 @@ mod postgresql;
 mod handlers;
 
 use std::env;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{ App, HttpServer};
 use postgresql::connect_postgres::PostgresPool;
 
 use crate::{postgresql::seeding::seeding, handlers::app::init};
 
-
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
