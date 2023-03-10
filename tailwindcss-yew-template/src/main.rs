dd604a3019ef2c4a6b90793ebcb6221f7b076c9c
Jinseok Seo <jinseok9338@gmail.com>
2023-03-10 22:52:38 +0900

    add Kakao init and authorize

--- tailwindcss-yew-template/src/main.rs
+++ tailwindcss-yew-template/src/main.rs
@@ -1,10 +1,14 @@
 extern crate reqwest_wasm;
 
+use js_sys::Reflect;
+use web_sys::HtmlScriptElement;
 use yew::prelude::*;
 use dotenv::dotenv;
 use std::env;
+use wasm_bindgen::{prelude::{wasm_bindgen, Closure}, JsValue, JsCast};
+
 mod data;
-use serde_json::{json, to_string};
+
 
 enum Msg {
     FetchNewUser,
@@ -92,6 +96,10 @@ impl Component for App {
     fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
         if first_render {
             ctx.link().send_future(App::get_person());
+            match App::init_kakao() {
+                Ok(_) => log::info!("Kakao init success"),
+                Err(_) => log::info!("Kakao init failed"),
+            }
         }
     }
 }
@@ -108,11 +116,51 @@ impl App {
         let parsed_json = serde_json::from_str::<data::Root>(res.as_str()).unwrap();
         return Msg::UpdatePerson((*parsed_json.results.first().unwrap()).clone());
     }
+
+
+   
+    pub fn init_kakao() -> Result<(), JsValue> {
+        let window = web_sys::window().expect("no global `window` exists");
+        let document = window.document().expect("should have a document on window");
+        let body = document.body().expect("document should have a body");
+    
+        // create script element
+        let script = document.create_element("script")?.dyn_into::<HtmlScriptElement>()?;
+        script.set_src("https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js");
+        // append script to body
+        // add event listener for load event
+        let closure = Closure::wrap(Box::new(|| {
+            // define Kakao object here
+            let global = js_sys::global();
+            let kakao = Reflect::get(&global, &JsValue::from_str("Kakao")).expect("Kakao object not found");
+            let init_method = Reflect::get(&kakao, &JsValue::from_str("init")).expect("init method not found");
+            let init_fn = init_method.dyn_ref::<js_sys::Function>().expect("Failed to cast init method to Function");
+            let key = JsValue::from_str("894b7703bad3d3d3641c1d06c9bde7fc");
+            init_fn.call1(&kakao, &key).expect("Failed to call init method");
+        }) as Box<dyn Fn()>);
+
+        script.add_event_listener_with_callback("load", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+        body.append_child(&script)?;
+        Ok(())
+    }
+
     async fn login_request() -> String   {
         dotenv().ok();
         
         let server_url: &str = env!("SERVER_URL");
-     
+        let global = js_sys::global();
+        let kakao = Reflect::get(&global, &JsValue::from_str("Kakao")).expect("Kakao object not found");
+        let auth = Reflect::get(&kakao, &JsValue::from_str("Auth")).expect("Auth object not found");
+        let authorize_method = Reflect::get(&auth, &JsValue::from_str("authorize")).expect("authorize method not found");
+        let authorize_fn = authorize_method.dyn_ref::<js_sys::Function>().expect("Failed to cast authorize method to Function");
+    
+        let redirect_uri = JsValue::from_str("http://127.0.0.1:3000");
+        let options = js_sys::Object::new();
+        Reflect::set(&options, &JsValue::from_str("redirectUri"), &redirect_uri).expect("Failed to set redirectUri option");
+    
+        authorize_fn.call1(&auth, &options).expect("Failed to call authorize method");
+
         let res = reqwest_wasm::Client::new()
         .get(format!("{}/login", server_url))
         .header("Content-Type", "application/json")
