e310b7e28f27a4a9d7a1a90b789911af476601e7
Jinseok Seo <jinseok9338@gmail.com>
2023-03-11 11:28:43 +0900

    small change

--- backend/src/handlers/app.rs
+++ backend/src/handlers/app.rs
@@ -1,6 +1,6 @@
-use std::env;
 
-use actix_web::{web, HttpResponse, Responder, HttpRequest, http::header};
+
+use actix_web::{web, HttpResponse, Responder, HttpRequest,};
 
  async fn hello(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -14,23 +14,7 @@ use actix_web::{web, HttpResponse, Responder, HttpRequest, http::header};
     HttpResponse::Ok().body("Hey there!")
 }
 
-async fn login_request(_req: HttpRequest ) -> impl Responder {
-   // send get request to  kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code
-   // get REST_API_KEY from env variable
-   // get REDIRECT_URI from env variable
-   dotenv::dotenv().ok();
-   let redirect_uri = env::var("REDIRECT_URI").unwrap();
-   let rest_api_key = env::var("REST_API_KEY").unwrap();
 
-    let url = format!("https://kauth.kakao.com/oauth/authorize?client_id={}&redirect_uri={}&response_type=code", rest_api_key, redirect_uri);
-    // send get request to url
-    let res = reqwest::get(url)
-    .await.unwrap()
-    .text()
-    .await.unwrap();
-
-    HttpResponse::Ok().body(res)
-}
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
@@ -38,10 +22,7 @@ pub fn init(cfg: &mut web::ServiceConfig) {
             .route(web::get().to(hello))
             .route(web::post().to(echo))
     );
-    cfg.service(
-        web::resource("/login")
-            .route(web::get().to(login_request))
-    );
+
 
     cfg.route("/hey", web::get().to(manual_hello));
 }
